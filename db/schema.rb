e4aba4454e8bd7bcd1ebc51b47b66694aeb23142
Prakash Pradhan <prakash.pradhan010@gmail.com>
2016-03-15 09:34:08 +0530

    column renamed from picture to photo in categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160218125634) do
+ActiveRecord::Schema.define(version: 20160315025500) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name", limit: 255
-    t.string   "picture",       limit: 255
+    t.string   "photo",         limit: 255
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
   end
