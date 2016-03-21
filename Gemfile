df7293a5d45d62b380cf1c9557cf33bb5ae5819c
Prakash Pradhan <prakash.pradhan010@gmail.com>
2016-03-21 21:33:42 +0530

    carrierwave modified

--- Gemfile
+++ Gemfile
@@ -36,8 +36,7 @@ gem 'formtastic', '3.0'
 gem 'rmagick'
 
 #gem 'net-ssh'
-gem  'fog', require: 'fog/aws'
-
+gem  'fog'
 gem 'foundation-rails'
 #gem "twitter-bootstrap-rails"
 # Use ActiveModel has_secure_password
--- config/initializers/s3.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: 'AKIAIDREYTER5N445KYQ',
-    aws_secret_access_key: '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',
-    region: 'Singapore'
-  }
-  config.fog_directory  = 'usar-s3'
-end
