075ee2f9090aa0157f5e77b3af50e242c6345dc8
pkp007 <pradhan_prakash007@yahoo.com>
2016-06-26 11:17:59 +0530

    Feedback and Rating implemented successfuly . Version 1 of API completed.

--- app/api/api/v1/feedbacks.rb
+++ app/api/api/v1/feedbacks.rb
@@ -6,20 +6,35 @@ module API
       default_format :json
 
       resources :feedbacks do
+
         desc 'get the latest feedbacks'
-        get '' do
-          Feedback.all
-        end
 
+        #sample url "localhost:3000/api/v1/feedbacks/122"
+        get ':place_id' do
 
-        get ':place_id/feedbacks' do
-          @feedback = Feedback.get_feedback_messages(params[:place_id])
+          params do
+            optional :filter, type: String, desc: "filter criteria"
+          end
+
+          if params[:filter].blank?
+            @feedback = Feedback.where(place_id: params[:place_id]).all
+          else
+            case params[:filter]
+              when "message"
+                Feedback.get_feedback_messages(params[:place_id])
+              when "rating"
+                Feedback.get_rating(params[:place_id])
+              else
+                @feedback = Feedback.where(place_id: params[:place_id]).all
+            end
+          end
+
         end
 
         desc 'Create a feedback.'
         params do
           requires :feedback, type: Hash do
-            requires :place_id , type: Integer, desc: "Id of the place on which feedback is given"
+            requires :place_id, type: Integer, desc: "Id of the place on which feedback is given"
             optional :email, type: String, desc: "Email Address of a user"
             optional :deviceID, type: String, desc: "Adroid device Id. "
             optional :message, type: String, desc: "feedback message."
@@ -28,11 +43,32 @@ module API
           end
         end
         post '' do
-          @feedback = Feedback.create_or_update_feedback(params[:feedback][:place_id],params[:feedback][:email], params[:feedback][:deviceID], params[:feedback][:message], params[:feedback][:rating_value], params[:feedback][:likes])
+          @feedback = Feedback.create_or_update_feedback(params[:feedback][:place_id], params[:feedback][:email], params[:feedback][:deviceID], params[:feedback][:message], params[:feedback][:rating_value], params[:feedback][:likes])
           status 200
           @feedback
         end
 
+      end
+
+      resources :like do
+
+        get ':place_id' do
+          @feedback = Feedback.get_no_of_likes(params[:place_id])
+        end
+
+
+        # TODO: Implement rating updates separately
+        params do
+          requires :like, type: Hash do
+            requires :place_id, type: Integer, desc: "Id of the place on which feedback is given"
+            optional :deviceID, type: String, desc: "Adroid device Id. "
+          end
+        end
+        post '' do
+          @feedback = Feedback.update_likes(params[:like][:place_id], params[:like][:deviceID])
+          status 200
+          @feedback
+        end
       end
 
     end
--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -3,25 +3,37 @@ class Feedback < ActiveRecord::Base
 
   def self.create_or_update_feedback (place_id, email, deviceID, message, rating_value, likes)
 
-    @feedback = Feedback.find_by_deviceID(deviceID)
+    @feedback = Feedback.where(deviceID: deviceID , place_id: place_id)
 
     if @feedback.present?
-      @feedback = Feedback.update_all(place_id, email: email, deviceID: deviceID, message: message, rating_value: rating_value, likes: likes)
+        # @feedback = Feedback.update( message: message, rating_value: rating_value)
+        @feedback.update_all( message: message, rating_value: rating_value)
+
     else
       @feedback = Feedback.create!(place_id: place_id, email: email, deviceID: deviceID, message: message, rating_value: rating_value, likes: likes)
     end
   end
 
   def self.get_feedback_messages(place_id)
-    @feedback = Feedback.select(:message).where(place_id: place_id).order('id DESC')
+  @feedback = Feedback.select(:message).where(place_id: place_id).order('id DESC')
   end
 
   def self.get_rating(place_id)
-    @feedback = Feedback.average(:rating_value).where(place_id: place_id)
+  @feedback = Feedback.where(place_id: place_id).average(:rating_value)
   end
 
   def self.get_no_of_likes (place_id)
-    @feedback = Feedback.where(place_id: place_id, likes: true).count()
+  @feedback = Feedback.where(place_id: place_id, likes: true).count()
   end
 
+  def self.update_likes (place_id,deviceID)
+    @feedback = Feedback.where(deviceID: deviceID , place_id: place_id)
+
+    if @feedback.present?
+      @feedback.update_all(likes:true)
+    else
+      @feedback = Feedback.create!(place_id: place_id,deviceID: deviceID, likes: true )
+    end
+
+  end
 end
